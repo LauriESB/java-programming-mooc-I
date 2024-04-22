db26a89698b6d5cdc6310abfb84796060d16e71f
Lauri Ellen <ich.lauri@gmail.com>
2024-04-22 20:14:15 -0300

    Upload Part01.SecondsInADay

---
+++ Part01/part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class SecondsInADay {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many days would you like to convert to seconds?");
+        int days = Integer.valueOf(scanner.nextLine());
+        
+        int seconds = days * 86400;
+        
+        System.out.println(seconds);
+
+    }
+}
