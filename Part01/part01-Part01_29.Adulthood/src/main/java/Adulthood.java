3d2fea72b1e01dcc32e7cdbb09574caf07cfc5f4
Lauri Ellen <ich.lauri@gmail.com>
2024-04-22 21:01:09 -0300

    Upload Part01.Adulthood

---
+++ Part01/part01-Part01_29.Adulthood/src/main/java/Adulthood.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Adulthood {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("How old are you?"); 
+        int age = Integer.valueOf(scan.nextLine());
+        
+        if (age < 18) {
+            System.out.println("You are not an adul");
+        } else {
+            System.out.println("You are an adult");
+        }
+        
+    }
+}
