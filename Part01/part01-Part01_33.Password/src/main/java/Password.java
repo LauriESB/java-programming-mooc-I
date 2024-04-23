9c7611fc88a57e6acd200af813348e8d4eeebeb8
Lauri Ellen <ich.lauri@gmail.com>
2024-04-22 21:19:07 -0300

    Upload Part01.Password

---
+++ Part01/part01-Part01_33.Password/src/main/java/Password.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Password {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Password?");
+        String pwrd =scan.nextLine();
+        
+        if (pwrd.equals("Caput Draconis")){
+            System.out.println("Welcome!");
+        }else{
+            System.out.println("Off with you!");
+        }
+    }
+}
