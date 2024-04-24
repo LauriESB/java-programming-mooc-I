243fa734b59187ca2e62d9b17ef8678ed6339ae8
Lauri Ellen <ich.lauri@gmail.com>
2024-04-23 23:09:02 -0300

    Upload Part02.AbsoluteValue

--- Part02/part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ Part02/part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -7,7 +7,7 @@ public class AbsoluteValue {
         
         // Write a program that reads an integer from the user. If the number is 
         // less than 0, the program prints the given integer multiplied by -1. 
-        // In all other cases, the program prints the number itself.
+        // In all other cases, the program prints the number itself..
         
         Scanner scanner = new Scanner(System.in);
         
