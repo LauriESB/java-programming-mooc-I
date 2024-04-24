85d11fd9b760403605d667703ebcc218fce4543f
Lauri Ellen <ich.lauri@gmail.com>
2024-04-23 23:12:20 -0300

    Upload Part02.ComparingNumbers

--- Part02/part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
+++ Part02/part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
@@ -9,7 +9,7 @@ public class ComparingNumbers {
         // If the first number is greater than the second, the program prints "(first) is greater than (second)." 
         // If the first number is less than the second, the program prints "(first) is smaller than (second)." 
         // Otherwise, the program prints "(first) is equal to (second)." 
-        // The (first) and (second) should always be replaced with the actual numbers that were provided by the user.
+        // The (first) and (second) should always be replaced with the actual numbers that were provided by the user..
         
         Scanner scanner = new Scanner(System.in);
 
