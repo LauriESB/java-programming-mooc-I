c4abbae67678f68613be9195b9e35ebaf4c324a2
Lauri Ellen <ich.lauri@gmail.com>
2024-04-29 23:41:05 -0300

    Upload Part02.InAHoleInTheGround

---
+++ Part02/part02-Part02_21.InAHoleInTheGround/src/main/java/InAHoleInTheGround.java
@@ -0,0 +1,12 @@
+public class InAHoleInTheGround {
+
+    // Create a method called printText which prints the phrase "In a hole in the ground there lived a method" and a newline.
+    
+    public static void main(String[] args) {
+        printText();
+    }
+    
+    public static void printText() {
+        System.out.println("In a hole in the ground there lived a method");
+    }
+}
