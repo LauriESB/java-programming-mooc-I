0b03e79d5f66e9627a325452fe6c6c5dc2c03e56
Lauri Ellen <ich.lauri@gmail.com>
2024-04-30 00:02:30 -0300

    Upload Part02.NumberUno

---
+++ Part02/part02-Part02_27.NumberUno/src/main/java/NumberUno.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class NumberUno {
+    
+    // Write a method public static int numberUno() that returns the value 1.
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        numberUno();
+    }
+    public static int numberUno() {
+        return 1;
+    }
+
+}
