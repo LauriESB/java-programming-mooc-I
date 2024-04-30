66cc3ccfd55295fc7ddb8f99216d31f3fc7ef54b
Lauri Ellen <ich.lauri@gmail.com>
2024-04-30 19:21:43 -0300

    Upload Part02.Word

---
+++ Part02/part02-Part02_28.Word/src/main/java/Word.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Word {
+
+    // Write a method public static String word(). 
+    // The method must return a string of your choice.
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        word();
+    }
+    
+    public static String word() {
+        return "Not me";
+    }
+
+}
