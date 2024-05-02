b8be623ca8d90470d5411fc7101b0cb2e11a9087
Lauri Ellen <ich.lauri@gmail.com>
2024-05-02 20:59:57 -0300

    Upload Part04.Debt

---
+++ Part04/part04-Part04_09.Debt/src/main/java/Debt.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * @author ichla
+ */
+public class Debt {
+    private double balance;
+    private double interestRate;
+    
+    public Debt(double initialBalance, double initialInterestRate) {
+        this.balance = initialBalance;
+        this.interestRate = initialInterestRate;
+    }
+    public void printBalance() {
+        System.out.println(this.balance);
+    }
+    public void waitOneYear() {
+        this.balance = this.balance * this.interestRate;
+    
+    }
+}
