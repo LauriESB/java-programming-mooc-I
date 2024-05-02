2761db1f6ef2d00f85ee50475a545c82888a95c7
Lauri Ellen <ich.lauri@gmail.com>
2024-05-01 23:28:16 -0300

    Upload Part03.GreatestInList

---
+++ Part03/part03-Part03_09.GreatestInList/src/main/java/GreatestInList.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GreatestInList {
+    
+    // Continue developing the program so that it finds the greatest number in the list and prints its value after reading all the numbers.
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+
+            list.add(input);
+        }
+        int greatest = list.get(0);
+        
+        for (int i = 0; i < list.size(); i++) {
+            int number = list.get(i);
+            if(number >= greatest) {
+                greatest = number;
+            }
+        }
+        System.out.println("The greatest number: " + greatest);
+
+        // implement finding the greatest number in the list here
+    }
+}
