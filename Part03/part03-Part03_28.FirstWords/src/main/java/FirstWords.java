96be0e4e6df9d4c7b66a54a2f910a5a4b7230b18
Lauri Ellen <ich.lauri@gmail.com>
2024-05-02 19:00:54 -0300

    Upload Part03.FirstWords

---
+++ Part03/part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class FirstWords {
+    
+    // Write a program that reads user input until an empty line is entered. 
+    // For each non-empty line the program splits the string by spaces  and prints the first part of the string.
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            
+            String word = scanner.nextLine();
+            
+            if(word.equals("")) {
+                break;
+            }
+            
+            String[] parts = word.split(" ");
+            System.out.println(parts[0]);
+        
+        
+        }
+
+    }
+}
