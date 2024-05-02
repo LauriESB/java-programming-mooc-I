ced5ce85129ad1586753db820f312f2a5847654d
Lauri Ellen <ich.lauri@gmail.com>
2024-05-02 20:42:02 -0300

    Upload Part04.Room

---
+++ Part04/part04-Part04_04.Room/src/main/java/MainProgram.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class MainProgram {
+    
+    // Create a class named Room. Add the variables private String code and private int seats to the class. 
+    // Then create a constructor public Room(String classCode, int numberOfSeats) through which values are assigned to the instance variables.
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);      
+    }
+}
---
+++ Part04/part04-Part04_04.Room/src/main/java/Room.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author ichla
+ */
+public class Room {
+    private String code;
+    private int seats;
+    
+    public Room(String classCode, int numberOfSeats) {
+        this.code = classCode;
+        this.seats = numberOfSeats;
+    }
+}
