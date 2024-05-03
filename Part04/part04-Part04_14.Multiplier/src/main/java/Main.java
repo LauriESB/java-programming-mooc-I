a1d578522dd53174de2cea1f0cc565f8d15e41a7
Lauri Ellen <ich.lauri@gmail.com>
2024-05-02 23:10:09 -0300

    Upload Part04.multiply

---
+++ Part04/part04-Part04_14.Multiplier/src/main/java/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        // Create a class Multiplier that has a:
+        // Constructor public Multiplier(int number).
+        // Method public int multiply(int number) which 
+        // returns the value number passed to it multiplied by the number provided to the constructor.
+
+
+        Multiplier multiplyByThree = new Multiplier(3);
+        
+        System.out.println("multiplyByThree.multiply(2): " + multiplyByThree.multiply(2));
+        
+        Multiplier multiplyByFour = new Multiplier(4);
+        System.out.println("multiplyByFour.multiply(2): " + multiplyByFour.multiply(2));
+        System.out.println("multiplyByThree.multiply(1): " + multiplyByThree.multiply(1));
+        System.out.println("multiplyByFour.multiply(1): " + multiplyByFour.multiply(1));
+        
+    }
+}
