40873bdd2dfdd7f54478d1eedcecce9f65e40018
Lauri Ellen <ich.lauri@gmail.com>
2024-05-03 23:03:37 -0300

    Upload Part04.PrintingAFile

---
+++ Part04/part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
@@ -0,0 +1,24 @@
+
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class PrintingAFile {
+    
+    // Write a program that prints the contents of a file called "data.txt", 
+    // such that each line of the file is printed on its own line.
+
+    public static void main(String[] args) {
+
+        try (Scanner scanner = new Scanner(Paths.get("data.txt"))) {
+        
+            while (scanner.hasNextLine()) {
+            
+                String row = scanner.nextLine();
+                System.out.println(row);
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
