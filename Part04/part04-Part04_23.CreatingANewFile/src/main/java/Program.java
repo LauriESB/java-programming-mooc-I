32278b5832e388d53006ac6a54b8f7a9eca5b403
Lauri Ellen <ich.lauri@gmail.com>
2024-05-03 22:45:45 -0300

    Upload Part04.CreatingANewFile

---
+++ Part04/part04-Part04_23.CreatingANewFile/src/main/java/Program.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author LauriESB
+ */
+
+public class Program {
+    
+    // NB! In this exercise, we won't be programming. 
+    // Instead, you'll familiarize yourself with the Files-tab in NetBeans and how to create a new file.
+    // Create a file called file.txt in the root folder (the folder containing the folder src and the file pom.xml) 
+    // of the exercise template using the Files-tab in NetBeans. 
+    // Edit the file and write the message Hello, world! on the first line of the file.
+
+    public static void main(String[] args) {
+        
+    }
+}
